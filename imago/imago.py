83a442b2dd2a8b31461b334516260080d01bf5cd
Divyanshu_S <singhdivyanshu010@gmail.com>
2020-05-22 17:00:23 +0530

    Update imago.py

--- imago/imago.py
+++ imago/imago.py
@@ -8,14 +8,7 @@ import helper
 from os import walk
 
 def main(args=None):
-	print("""\
-##################################################
-# imago.py                                       #
-# Digital evidences from images!                 #
-# Made with <3 by Matteo Redaelli                #
-# Twitter: @solventred                           #
-##################################################
-	""")
+
 	if args is None:
 		args = sys.argv[1:]
 	parser = argparse.ArgumentParser()
